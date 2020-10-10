0e6cb546a9af71103b03553366585bb2abfae1b9
Sia <mbatia@unlv.nevada.edu>
2020-10-10 11:04:00 -0700

    updated data function structure

--- data_slices.py
+++ data_slices.py
@@ -1,13 +1,16 @@
 #module to prepare data for evaluation and model generation
 
+def load_data():
+    pass
+
 class EvaluationData():
     def __init__(self):
+        self.full_dataset = load_data()
+
+    def train_dataset(self):
         pass
 
-    def get_train_dataset(self):
-        pass
-
-    def get_test_dataset(self):
+    def test_dataset(self):
         pass
 
     def full_train_dataset(self):
