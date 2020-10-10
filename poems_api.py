9c583287d19a249eefb0d9674a3b729893ade027
Sia <mbatia@unlv.nevada.edu>
2020-10-09 22:45:05 -0700

    set structure of API return functions

---
+++ poems_api.py
@@ -0,0 +1,19 @@
+#modules
+import json
+import requests
+
+class Poem():
+    def __init__(self):
+        pass
+
+    def get_poem_data(self, **kwargs):
+        pass
+
+    def get_poems_by_poet(self, author):
+        pass
+
+    def get_poems_by_theme(self, theme):
+        pass
+
+    def get_poems_by_keyword(self, words):
+        pass
