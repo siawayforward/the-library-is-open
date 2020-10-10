4fdbd4c726780485dd4eed7eddf2bafff6071a80
Sia <mbatia@unlv.nevada.edu>
2020-10-09 22:44:34 -0700

    set metrics calculation class structure

---
+++ recommender_metrics.py
@@ -0,0 +1,48 @@
+#module including all methods that calculate the effectiveness of a recommender system
+
+class RecommenderMetrics():
+    def __init__(self):
+        #self.actual_values
+        #self.predicted_values 
+        #get data in all needed forms from the evaluation data module
+        pass
+
+    #mean absolute error
+    def MAE(self):
+        pass
+
+    #root mean square error
+    def RMSE(self):
+        pass
+
+    def hit_rate(self):
+        pass
+
+    #leave one-out cross validation
+    def leave_one_out_CV(self):
+        pass
+    
+    def average_reciprocal_hit_rank(self):
+        pass
+
+    def cumulative_hit_rate(self):
+        pass
+
+    def rating_hit_rate(self):
+        pass
+
+    def coverage(self):
+        pass
+
+    def diversity(self):
+        pass
+
+    def novelty(self):
+        pass
+
+    def churn(self):
+        pass
+
+    def responsiveness(self):
+        pass
+
