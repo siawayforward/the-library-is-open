04ad7266d222e713f77c6bb1d668d15c48271147
Sia <mbatia@unlv.nevada.edu>
2020-10-09 22:44:49 -0700

    set data slice class structure

---
+++ data_slices.py
@@ -0,0 +1,19 @@
+#module to prepare data for evaluation and model generation
+
+class EvaluationData():
+    def __init__(self):
+        pass
+
+    def get_train_dataset(self):
+        pass
+
+    def get_test_dataset(self):
+        pass
+
+    def full_train_dataset(self):
+        pass
+
+    def anti_test_dataset(self):
+        pass
+    
+    
