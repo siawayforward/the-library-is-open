4c6ad5fb40cf91e68bdfcd6ad1e78913ac5e737f
Sia <mbatia@unlv.nevada.edu>
2020-10-23 22:44:35 -0700

    added module to read in api keys

---
+++ retrieve_api_keys.py
@@ -0,0 +1,9 @@
+#get keys for both API requests
+
+def get_API_keys():
+    with open('keys.txt', 'r') as file:
+        key_value_pairs = ' '.join(file.readlines()).replace('\n', '')
+        nyt_key = key_value_pairs.split('google,')[0].split('nyt,')[1].strip()
+        google_key = key_value_pairs.split('google, ')[1].strip()
+        file.close()
+    return {nyt_key, google_key}
