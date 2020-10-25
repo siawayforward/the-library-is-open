9e36cd57fc63d9fe9818fa845aa20ae9e910d926
Sia <mbatia@unlv.nevada.edu>
2020-10-25 00:21:02 -0700

    updated cosine similarity calculation

--- book_similarity.py
+++ book_similarity.py
@@ -1,5 +1,5 @@
 #modules
-from nyt_bestsellers import TimesBestsellers, Book
+from nyt_bestsellers import TimesBestsellers
 from time import time
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -12,27 +12,31 @@ class BookSimilarity:
 
     def __init__(self):
         start = time()
-        self.books = TimesBestsellers().get_book_list()
+        nyt = TimesBestsellers()
+        self.books = nyt.get_book_list()
         # check computation time
         m, s = str(int((time() - start)//60)), int((time() - start)%60)
         print('Time: {}:{:02d}m - done getting books history from the all lists\n'.format(m,s))
-        print('Total books:', str(len(books.all_books))) 
+        print('Total books:', str(len(self.books))) 
 
     
     def get_book_recommendations(self, title, author, top_n=10):
         '''
             Get the `top_n` number of recommendations for given `book_title` and `author`
         '''
-        
+        TimesBestsellers().find_book(title, author)
+        self.compute_feature_similarities()
+        target = self.book_df[(self.book_df['title'] == title) & self.book_df['author'] == author]
+        recommendations = self.similarity_scores[target.index.item()]
+        self.top_recommendations = sorted(recommendations, key=lambda x: x[1])[:top_n]
+        return self.top_recommendations
 
-       
+
     def compute_feature_similarities(self):
         self.create_books_dataframe()
         self.vectorize_text_features()
-        similarity_scores = cosine_similarity(self.vector_df)
-        self.sorted_similarities = sorted(self.sorted_similarities)
-        #revisit this method - completed when you were sleepy
-        
+        self.similarity_scores = cosine_similarity(self.vector_df)
+
 
     def vectorize_text_features(self):
         # create tfidf vectors
@@ -48,8 +52,8 @@ class BookSimilarity:
             item = {'title': book.title, 'author': book.author, 'description': book.description, 
                     'target': book.title + ' ' + book.description, 'isbn13': book.primary_isbn13}
             book_list.append(item)
-        # create dataframe and have isbn as index
-        self.book_df = pd.DataFrame(book_list).set_index('isbn13')
+        # create dataframe of data
+        self.book_df = pd.DataFrame(book_list)
 
     
     def pre_process_book_data(self, book):
@@ -60,20 +64,3 @@ class BookSimilarity:
                                     if w not in stopwords.words('english')])
         # creating dataframe with title, author, synopsis before lemmatizing, vectorizing
         return book    
-
-
-'''
-    DONE - get the list of list names
-    get the list of books from the lists (only store ISBN) 
-    DONE - how do we get all lists from the beginning of time?
-    use descriptions, genre to make recommendations
-    DONE - we could make tdidf vectorizers and use those to calculate cosine-similarity
-    - suggest top 10 books with highest similarity scores
-    engine:
-    - you enter a book title and author
-    - we search the repo of books from NYT best sellers and give a recommendation
-    - if book not in New York Times list, search for book data in open library API
-        = From there, get details of book and then add it to the book_df
-        = calculate its similarity score and add to the similarities list
-
-'''
