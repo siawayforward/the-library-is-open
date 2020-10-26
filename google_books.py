002920ba03c43363b1f117dc4516b4bc5b0445fb
Sia <mbatia@unlv.nevada.edu>
2020-10-25 23:37:32 -0700

    changed location of findbooks method

--- google_books.py
+++ google_books.py
@@ -1,5 +1,6 @@
 import requests
-from retrieve_api_keys import *
+import pandas as pd
+from retrieve_api_keys import get_API_keys
 
 class GoogleBooks:
     '''
@@ -8,11 +9,11 @@ class GoogleBooks:
         reader
     '''
     def __init__(self, title=None, author=None):
-        self.headers = {
-            'api-key': get_API_keys[1], 'q': None, 'inauthor': None}
+        self.headers = {'api-key': get_API_keys()[1], 'q': None, 'inauthor': None}
         self.book_ep = 'https://www.googleapis.com/books/v1/volumes'
         if title: self.headers['q'] = title
         if author: self.headers['inauthor'] = author
+        self.find_book(title, author)
 
     
     def get_book(self):
@@ -20,13 +21,30 @@ class GoogleBooks:
         try:
             response = requests.get(self.book_ep, params=self.headers)
             top_result = response.json()['items'][0]['volumeInfo']
-            retrieved_book = Book(
-                title = top_result['title'][0],
-                author = ', '.join(top_result['authors'])[0],
-                description = top_result['description'][0],
-                primary_isbn13 = top_result['industryIdentifiers'][0]['identifier']
-            )
+            retrieved_book = {
+                'title': top_result['title'][0],
+                'author': ', '.join(top_result['authors'])[0],
+                'description': top_result['description'][0],
+                'primary_isbn13': top_result['industryIdentifiers'][0]['identifier']
+            }
         except: 
             print('Failed to find or retrieve book requested.')
             print('Recommendation will be based on title and author only')
         return retrieved_book
+
+
+    def find_book(self, title, author):
+        books = pd.read_csv('books.csv')
+        titles = list(books.title)
+        authors = list(books.author)
+        if title in titles and authors[titles.index(title)] == author: 
+            pass
+        else: #append to dataframe to update corpus
+            retrieved_book = self.get_book()
+            if retrieved_book: books.append(retrieved_book, ignore_index=True)
+            else:
+                books.append(
+                    {'title': title, 'author': author, 'description': '', 'primary_isbn13': 0},
+                    ignore_index = True)
+        #resave data
+        books.to_csv('books.csv', index=False)
