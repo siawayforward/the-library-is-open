20bc46971167bae2a79888d9b25dc894c295aee4
Sia <mbatia@unlv.nevada.edu>
2020-10-28 00:07:14 -0700

    updated amount and layout of recommendations

--- recommend_a_book.py
+++ recommend_a_book.py
@@ -10,26 +10,27 @@ class BookRecommender:
         if update:
             BookSimilarity().update_book_corpus()
 
+
     def display_recommendations(self):
         bs = BookSimilarity()
         title, author, top_n = self.get_user_conditions()
         self.recommendations = bs.get_book_recommendations(title, author, top_n)
-
-        print('We think these titles are a great follow-up to {} by {}:'
-                .format(title, author))
+        print('We think these titles are great follow-ups to {} by {}:'
+                .format(title.title(), author.title()))
         for book in self.recommendations:
-            book_row = self.book_sim.book_df.iloc[book[0]] 
-            print(book_row['title'].title(),'by', book_row['author'].title())
+            book_row = bs.book_df.iloc[book[0]] 
+            if int(book[1]) != 1:
+                print(book_row['title'].title(),'by', book_row['author'].title())
 
 
     def get_user_conditions(self):
         title, author = input('What book have you read recently?\n'), None
         while title is None:
             title = input('Uh Oh! We didn\'t get that. What book did you read?\n')
-            author = input('OK! What\'re the author\'s first and last names?\n')
-            while author is None:
-                author = input('We missed that author! Who wrote {} again?\n'.format(title))     
-        return title, author, 5
+        author = input('OK! What\'re the author\'s first and last names?\n')
+        while author is None:
+            author = input('We missed that author! Who wrote {} again?\n'.format(title))     
+        return title, author, 6
 
 
 
