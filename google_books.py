d314b5c5475cfca001ff90cedf009a8028af143d
Sia <mbatia@unlv.nevada.edu>
2022-01-06 18:03:12 -0500

    update google supplement functions

--- google_books.py
+++ google_books.py
@@ -1,6 +1,11 @@
 import requests
 import pandas as pd
 from retrieve_api_keys import get_API_keys
+from pprint import PrettyPrinter
+
+
+
+pp = PrettyPrinter(indent=4)
 
 class GoogleBooks:
     '''
@@ -13,7 +18,6 @@ class GoogleBooks:
         self.book_ep = 'https://www.googleapis.com/books/v1/volumes'
         if title: self.headers['q'] = title
         if author: self.headers['inauthor'] = author
-        self.find_book(title, author)
 
     
     def get_book(self):
@@ -21,30 +25,30 @@ class GoogleBooks:
         try:
             response = requests.get(self.book_ep, params=self.headers)
             top_result = response.json()['items'][0]['volumeInfo']
+            rating = None
+            if 'averageRating' in top_result.keys():
+                rating = top_result['averageRating']
             retrieved_book = {
-                'title': top_result['title'][0],
-                'author': ', '.join(top_result['authors'])[0],
-                'description': top_result['description'][0],
-                'primary_isbn13': top_result['industryIdentifiers'][0]['identifier']
+                'title': top_result['title'],
+                'author': ', '.join(top_result['authors']),
+                'description': top_result['description'],
+                'target': None,
+                'primary_isbn13': top_result['industryIdentifiers'][0]['identifier'],
+                'weeks': None,
+                'rating': rating,
+                'genre': ', '.join(top_result['categories'])
             }
+            print(retrieved_book)
         except: 
             print('Failed to find or retrieve book requested.')
             print('Recommendation will be based on title and author only')
         return retrieved_book
 
 
-    def find_book(self, title, author):
-        books = pd.read_csv('books.csv')
-        titles = list(books.title)
-        authors = list(books.author)
-        if title in titles and author.lower() in authors[titles.index(title)].lower(): 
-            pass
-        else: #append to dataframe to update corpus
-            retrieved_book = self.get_book()
-            if retrieved_book: books.append(retrieved_book, ignore_index=True)
-            else:
-                books.append(
-                    {'title': title, 'author': author, 'description': title, 'primary_isbn13': 0},
-                    ignore_index = True)
-        #resave data
-        books.to_csv('books.csv', index=False)
+
+    
+
+
+
+#gb = GoogleBooks(title='Vanishing Half')
+#gb.get_book()
