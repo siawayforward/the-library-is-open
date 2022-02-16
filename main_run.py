f11f4d289f6357aab1fdbde11a6c1db940c91dae
Sia <mbatia@unlv.nevada.edu>
2022-02-15 21:21:31 -0500

    change from log print to streamlit input fields

--- main_run.py
+++ main_run.py
@@ -1,46 +1,8 @@
-from google_books import GoogleBooks
-#from book_similarity import BookSimilarity
+from recommend_a_book import BookRecommender as br
 import pandas as pd
 import streamlit as st
 from PIL import Image
 
-
-books = pd.read_csv('all_books.csv')
-
-def find_book(title=None, author=None):
-    isbns = list(books.primary_isbn13)
-    gb = GoogleBooks(title, author)
-    retrieved_book = gb.get_book()
-    if int(retrieved_book['isbn13']) in isbns: 
-        print('book already there')
-    else: #append to dataframe to update corpus if it isn't already there
-        if retrieved_book: 
-            print('book found, adding...')
-            books.append(retrieved_book, ignore_index=True)
-            print('...done!')
-            books.to_csv('all_books.csv', index=False) 
-            print('corpus updated')
-        else:
-            print('book not found, not added')
-    return books
-
-
-def get_recommendations(title):
-    #see if book is in bestsellers list or can be retrieved from google books first
-    # this should all be after getting inputs so maybe set parameters for this method too.
-    # let's do this tomorrow, IM SO PROUD OF YOU SIA!
-    find_book(title)
-"""   
-      def get_user_conditions(self):
-            title, author = input('What book have you read recently?\n'), None
-        while title is None:
-            title = input('Uh Oh! We didn\'t get that. What book did you read?\n')
-        author = input('OK! What\'re the author\'s first and last names?\n')
-        while author is None:
-            author = input('We missed that author! Who wrote {} again?\n'.format(title))     
-        return title, author, 6  
-"""
-    
 
 # title for page
 st.title("The Library is Open!")
@@ -53,9 +15,43 @@ st.text("""
 image = Image.open('rupaul-library.gif')
 st.image(image, "Ru Paul noting to the queens in the reading challenge that the library is indeed about to be OPEN!")
 
+
 # create streamlit objects to interact with
+title = st.text_input('What book have you read recently?')
+author = st.text_input('OK! What\'re the author\'s first and last names?')
 
+# buttons
+search_button = st.button("Hit Me!!")
+reset_button = st.button("Can we start over?")
 
+if search_button:
+    while title=="":
+        title = st.text_input('Uh Oh! We didn\'t get that. What book did you read?')
+    while author=="":
+        author= st.text_input('We missed that author! Who wrote {} again?'.format(title))
 
-#test
-#get_recommendations(title='my sister the serial killer')
+# reset to start over
+if reset_button:
+    title = st.text_input('What book have you read recently?')
+    author = st.text_input('OK! What\'re the author\'s first and last names?')
+
+#see if book is in bestsellers list or can be retrieved from google books first
+loadstate = st.text('Let\'see about this one!')
+books = br.find_book(title, author)
+
+loadstate.text('Found your book! Finding you recommendations...')
+recommendations = br().display_recommendations(title, author)
+
+recommendations_header = 'We think these titles are great follow-ups to {} by {}:'.format(title, author)
+no_recs = 'Sorry, we couldn\'t find your next obsession this time :('
+
+if len(recommendations) > 1:
+    loadstate.text(recommendations_header)
+    # display recommendations
+    for book in recommendations:
+        book_row = books.iloc[book[0]] 
+        if int(book[1]) != 1:
+            st.write(book_row['title'].title(),'by', book_row['author'].title())
+else:
+    loadstate.text(no_recs)
+
