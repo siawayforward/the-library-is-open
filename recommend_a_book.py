ce038be784e892ae959502895a7304f069974b8e
Sia <mbatia@unlv.nevada.edu>
2022-02-15 19:13:39 -0500

    find book method in the wrapper

--- recommend_a_book.py
+++ recommend_a_book.py
@@ -1,4 +1,6 @@
 from book_similarity import BookSimilarity
+from google_books import GoogleBooks
+import pandas as pd
 
 class BookRecommender:
     '''
@@ -10,6 +12,25 @@ class BookRecommender:
     def __init__(self, update=False):
         if update:
             BookSimilarity().update_book_corpus()
+    
+    # check if corpus has this book before finding recommendations (come back to change print statements)
+    def find_book(title=None, author=None):
+        books = pd.read_csv('all_books.csv')
+        isbns = list(books.primary_isbn13)
+        gb = GoogleBooks(title, author)
+        retrieved_book = gb.get_book()
+        if int(retrieved_book['isbn13']) in isbns: 
+            print('book already there')
+        else: #append to dataframe to update corpus if it isn't already there
+            if retrieved_book: 
+                print('book found, adding...')
+                books.append(retrieved_book, ignore_index=True)
+                print('...done!')
+                books.to_csv('all_books.csv', index=False) 
+                print('corpus updated')
+            else:
+                print('book not found, not added')
+        return books
 
     # get recommendations based on what the user has entered
     def display_recommendations(self, book_title, book_author, n=6):
