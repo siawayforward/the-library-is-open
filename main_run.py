0b670dc0c394549ae632e6f7686b63759bcee8ef
Sia <mbatia@unlv.nevada.edu>
2022-02-26 21:42:24 -0500

    change streamlit elements into a form

--- main_run.py
+++ main_run.py
@@ -1,3 +1,4 @@
+from numpy import place
 from recommend_a_book import BookRecommender as br
 import pandas as pd
 import streamlit as st
@@ -8,7 +9,7 @@ from PIL import Image
 st.title("The Library is Open!")
 
 # description and image
-st.text("""
+st.write("""
         As a huge fan of Drag Race, the name of this is inspired by RuPaul's Drag Race. Ru, 
         I don't intend to make money from this so you still own 51% of that whole company, 
         ma'am. Much love!
@@ -16,43 +17,42 @@ st.text("""
 image = Image.open('rupaul-library.gif')
 st.image(image, "Ru Paul noting to the queens in the reading challenge that the library is indeed about to be OPEN!")
 
-
-# create streamlit objects to interact with
-title = st.text_input('What book have you read recently?')
-author = st.text_input('OK! What\'re the author\'s first and last names?')
-
-# buttons
-search_button = st.button("Hit Me!!")
-reset_button = st.button("Can we start over?")
-
-if search_button:
-    while title=="":
-        title = st.text_input('Uh Oh! We didn\'t get that. What book did you read?')
-    while author=="":
-        author= st.text_input('We missed that author! Who wrote {} again?'.format(title))
-
-# reset to start over
-if reset_button:
+# create streamlit objects to interact with   
+header = None 
+with st.form(key='ask_form', clear_on_submit=True):
     title = st.text_input('What book have you read recently?')
     author = st.text_input('OK! What\'re the author\'s first and last names?')
+    # buttons
+    search_button = st.form_submit_button("Hit Me!!")
 
-#see if book is in bestsellers list or can be retrieved from google books first
-loadstate = st.text('Let\'see about this one!')
-books = br.find_book(title, author)
+    if search_button:
+        if title=="" and author=="":
+            st.info("If you wanna be searching somethin, you got to be typing somethin (both title and author) hihiiii!")
+        elif (title=="" and author!="") or title=="":
+            st.info("We need us a book to search, friend! Please enter one")
+        elif author=="":
+            st.info("Who wrote {}? We didn't quite get that".format(title))
+        else:
+            #see if book is in bestsellers list or can be retrieved from google books first
+            loadstate = st.text('Let\'see about this one!')
+            books = br.find_book(title, author)
 
-loadstate.text('Found your book! Finding you recommendations...')
-recommendations = br().display_recommendations(title, author)
+            loadstate.text('Found your book! Finding you recommendations...')
+            recommendations = br().display_recommendations(title.lower(), author.lower())
 
-recommendations_header = 'We think these titles are great follow-ups to {} by {}:'.format(title, author)
-no_recs = 'Sorry, we couldn\'t find your next obsession this time :('
+            recommendations_header = 'We think these titles are great follow-ups to **{}** by **{}**:'\
+                .format(title.title(), author.title())
+            no_recs = 'Sorry, we couldn\'t find your next obsession this time :('
 
-if len(recommendations) > 1:
-    loadstate.text(recommendations_header)
-    # display recommendations
-    for book in recommendations:
-        book_row = books.iloc[book[0]] 
-        if int(book[1]) != 1:
-            st.write(book_row['title'].title(),'by', book_row['author'].title())
-else:
-    loadstate.text(no_recs)
+            if len(recommendations) > 1:
+                header = st.info(recommendations_header)
+                # display recommendations
+                for book in recommendations:
+                    book_row = books.iloc[book[0]] 
+                    if int(book[1]) != 1:
+                        st.write("-",book_row['title'].title(),'by', book_row['author'].title())
+            else:
+                st.info(no_recs)
 
+    # clearing results
+    reset_button = st.form_submit_button("Can we start over?")
